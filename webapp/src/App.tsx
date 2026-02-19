ab75de3a7abbe0e2d95945f108967f9ec606a9c1
uo288406 <clau1600@gmail.com>
2026-02-19 17:52:21 +0100

    Update App.tsx

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,34 +1,21 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-
-const API_URL = import.meta.env.VITE_GAMEY_URL || "http://localhost:4000";
+import RegisterForm from './RegisterForm';
+import reactLogo from './assets/react.svg'
 
 function App() {
-  const [status, setStatus] = useState("Checking server...");
-
-  useEffect(() => {
-    fetch(`${API_URL}/status`) 
-      .then(response => {
-        if (!response.ok) {
-          throw new Error("Server error");
-        }
-        return response.text();
-      })
-      .then(data => {
-        setStatus(data);
-      })
-      .catch(error => {
-        console.error("Error:", error);
-        setStatus("Server not reachable");
-      });
-  }, []);
-
   return (
     <div className="App">
-      <h2>Welcome to the Software Architecture 2025-2026 course</h2>
-      <h2>GameY coming soon</h2>
-      <h3>Rust Server status: {status}</h3>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
 
+      <h2>Welcome to the Software Arquitecture 2025-2026 course</h2>
+      <RegisterForm />
     </div>
   );
 }
