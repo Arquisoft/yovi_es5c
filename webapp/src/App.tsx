980fd3941c1dff85caba234a734dc2baa50dca58
uo288406 <clau1600@gmail.com>
2026-02-23 19:51:46 +0100

    adds

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,38 +1,19 @@
-import { useEffect, useState } from 'react'
+import {Routes, Route } from 'react-router-dom';  
+import { Box } from '@mui/material'; 
 import './App.css'
 import Register from "./pages/Register";
 
-const API_URL = import.meta.env.VITE_GAMEY_URL || "http://localhost:4000";
-
 function App() {
-  const [status, setStatus] = useState("Checking server...");
-
-  useEffect(() => {
-    fetch(`${API_URL}/status`) 
-      .then(response => {
-        if (!response.ok) {
-          throw new Error("Server error");
-        }
-        return response.text();
-      })
-      .then(data => {
-        setStatus(data);
-      })
-      .catch(error => {
-        console.error("Error:", error);
-        setStatus("Server not reachable");
-      });
-  }, []);
-
+   
   return (
-    <div className="App">
-      <h2>Welcome to the Software Architecture 2025-2026 course</h2>
-      <h2>GameY coming soon</h2>
-      <h3>Rust Server status: {status}</h3>
-
-      <Register />
-    </div>
-  );
+      <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh',gap: '0vh' }}>
+        
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        
+      </Box>
+  )
 }
 
 export default App;
