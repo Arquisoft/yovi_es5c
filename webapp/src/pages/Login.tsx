0f7c02b67ffd290aed2fe68a426b3291994fb9e9
josemzuvi <uo294061@uniovi.es>
2026-02-26 01:07:04 +0100

    Separado el css de Login en un fichero aparte.

--- webapp/src/pages/Login.tsx
+++ webapp/src/pages/Login.tsx
@@ -1,23 +1,11 @@
 import {Box} from "@mui/material";
 import LoginForm from "../components/LoginForm";
+import "./Login.css"
 
 const Login = () => {
 
   return (
-    // Fondo del viewport
-    <Box
-      sx={{
-        // Background
-        backgroundImage: "url(/fondo.jpg)",
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        // Distribucion de elementos
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "flex-start",
-      }}
-    >
+    <Box className="login">
       <LoginForm />
     </Box>
   );
