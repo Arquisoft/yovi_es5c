43b4e3034c9c307e9c016946a47a3111518273e7
josemzuvi <uo294061@uniovi.es>
2026-03-04 11:17:02 +0100

    Corregido idioma del login

--- webapp/src/components/LoginForm.tsx
+++ webapp/src/components/LoginForm.tsx
@@ -8,21 +8,21 @@ const LoginForm = () => {
 	<Container className="loginContainer"> 
 	<Paper className="loginCard" elevation={0} >
 		<Typography component="h1" variant="h4" align="center" gutterBottom>
-		Bienvenido
+		Welcome
 		</Typography>
 
-		<TextField required fullWidth label="Usuario" name="username" autoFocus />
-		<TextField required fullWidth name="password" label="Contraseña" type="password" />
+		<TextField required fullWidth label="User" name="username" autoFocus />
+		<TextField required fullWidth name="password" label="Password" type="password" />
 
 		<Button className="loginButton" type="submit" fullWidth variant="contained" size="large">
-		Iniciar Sesión
+		Log-In
 		</Button>
 
 		<Box className="formFooter">
 		<Typography variant="body1">
-			¿Aún no tienes cuenta?{" "}
+			You don't have an account yet?{" "}
 			<MuiLink component={Link} to="/register" underline="always">
-			Regístrate
+			Sign-up
 			</MuiLink>
 		</Typography>
 		</Box>
