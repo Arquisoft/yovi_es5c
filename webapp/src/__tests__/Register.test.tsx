f7987ead57ce19efa75dea204e2c156acc3c774d
Miguel Arias Guerrero <uo288285@uniovi.es>
2026-03-07 20:17:10 +0100

    Test sobre la página de personalización
    Se han hecho test sobre la página de personalización, de cuando se intenta entrar sin estar logueado, de cuando se crea la página y de cuando se pulsa en un botón y se va a otra página

--- webapp/src/__tests__/Register.test.tsx
+++ webapp/src/__tests__/Register.test.tsx
@@ -1,32 +1,32 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { describe, it, expect, vi, afterEach } from 'vitest';
-import Register from '../pages/Register';
-import { MemoryRouter } from 'react-router-dom';
-import '@testing-library/jest-dom';
-import { act } from 'react';
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { describe, it, expect, vi, afterEach } from "vitest";
+import Register from "../pages/Register";
+import { MemoryRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+import { act } from "react";
 
-import axios from 'axios';
+import axios from "axios";
 
-vi.mock('axios', () => ({
+vi.mock("axios", () => ({
   default: {
     post: vi.fn(),
   },
 }));
 
-vi.mock('../SessionContext', () => ({
+vi.mock("../SessionContext", () => ({
   useSession: () => ({
     isLoggedIn: false,
     createSession: vi.fn(),
   }),
 }));
 
-describe('Register page', () => {
+describe("Register page", () => {
   afterEach(() => {
     vi.clearAllMocks();
   });
 
-  it('renders the user registration title', () => {
+  it("renders the user registration title", () => {
     render(
       <MemoryRouter>
         <Register />
@@ -37,7 +37,7 @@ describe('Register page', () => {
     expect(title).toBeInTheDocument();
   });
 
-  it('submits form when data is correct', async () => {
+  it("submits form when data is correct", async () => {
     (axios.post as any).mockResolvedValue({ data: {} });
 
     await act(async () => {
@@ -76,14 +76,14 @@ describe('Register page', () => {
     /* ---------- USER ACTIONS ---------- */
 
     await act(async () => {
-      await user.type(usernameInput, 'miguel1235');
-      await user.type(nameInput, 'Miguel');
-      await user.type(surnameInput, 'Arias');
-      await user.type(emailInput, 'migueltest@test.com');
-      await user.type(passwordInput, 'Miguel1**');
-      await user.type(confirmPasswordInput, 'Miguel1**');
+      await user.type(usernameInput, "miguel1235");
+      await user.type(nameInput, "Miguel");
+      await user.type(surnameInput, "Arias");
+      await user.type(emailInput, "migueltest@test.com");
+      await user.type(passwordInput, "Miguel1**");
+      await user.type(confirmPasswordInput, "Miguel1**");
 
-      const registerButton = screen.getByRole('button', { name: /register/i });
+      const registerButton = screen.getByRole("button", { name: /register/i });
       await user.click(registerButton);
     });
 
@@ -92,7 +92,7 @@ describe('Register page', () => {
     });
   });
 
-  it('not put a field like username', async () => {
+  it("not put a field like username", async () => {
     await act(async () => {
       render(
         <MemoryRouter>
@@ -126,13 +126,13 @@ describe('Register page', () => {
     /* ---------- USER ACTIONS ---------- */
 
     await act(async () => {
-      await user.type(nameInput, 'f');
-      await user.type(surnameInput, 'f');
-      await user.type(emailInput, 'migueltest@test.com');
-      await user.type(passwordInput, 'Miguel1**');
-      await user.type(confirmPasswordInput, 'Miguel1**');
+      await user.type(nameInput, "f");
+      await user.type(surnameInput, "f");
+      await user.type(emailInput, "migueltest@test.com");
+      await user.type(passwordInput, "Miguel1**");
+      await user.type(confirmPasswordInput, "Miguel1**");
 
-      const registerButton = screen.getByRole('button', { name: /register/i });
+      const registerButton = screen.getByRole("button", { name: /register/i });
       await user.click(registerButton);
     });
 
@@ -141,7 +141,7 @@ describe('Register page', () => {
     expect(screen.getByText(/required field/i)).toBeInTheDocument();
   });
 
-  it('shows password error when password is invalid', async () => {
+  it("shows password error when password is invalid", async () => {
     render(
       <MemoryRouter>
         <Register />
@@ -154,8 +154,9 @@ describe('Register page', () => {
       'input[name="password"]'
     ) as HTMLInputElement;
 
-    await user.type(passwordInput, '123');
-
+    await act(async () => {
+      await user.type(passwordInput, "123");
+    });
     expect(
       await screen.findByText(/must have at least 8 characters/i)
     ).toBeInTheDocument();
