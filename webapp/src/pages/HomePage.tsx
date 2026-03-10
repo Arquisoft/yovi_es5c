3ba42cc5f777540420de8755604463bc284ef230
uo288406 <clau1600@gmail.com>
2026-03-10 17:25:14 +0100

    Update HomePage.tsx

--- webapp/src/pages/HomePage.tsx
+++ webapp/src/pages/HomePage.tsx
@@ -47,7 +47,7 @@ export default function HomePage() {
 
         <PlayButton 
             variant="contained" 
-            onClick={() => navigate('/game')}
+            onClick={() => navigate('/set')}
             startIcon={<PlayArrowIcon />}
           >
             Play
