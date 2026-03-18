406f58b059548cb50a0379302fc1ee00a321052d
Daniel González <danielgp477@gmail.com>
2026-03-18 15:02:13 +0100

    proteccion explicita de entradda de username en login

--- users/users-service.js
+++ users/users-service.js
@@ -75,6 +75,11 @@ app.post('/login', async (req, res) => {
     try {
         const { username, password } = req.body;
 
+
+        if (typeof username !== 'string' || typeof password !== 'string') {
+        return res.status(400).json({ error: 'Invalid input format.' });
+        }
+
         //Username and password must be filled
         if(!username || !password) {
             return res.status(400).json({ error: 'Username and password are required.' });
