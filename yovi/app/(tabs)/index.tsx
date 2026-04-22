44a5cb539b0cb9b84d29330a14dcfd358019ec07
claudiaRFS <clau1600@gmail.com>
2026-04-22 15:56:46 +0200

    cambio view

--- yovi/app/(tabs)/index.tsx
+++ yovi/app/(tabs)/index.tsx
@@ -1,16 +1,20 @@
-import { WebView } from 'react-native-webview';
 import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { WebView } from 'react-native-webview';
 
 export default function HomeScreen() {
   return (
-    <WebView
-      source={{ uri: 'http://4.233.210.164' }}
-      style={styles.webview}
-    />
+    <SafeAreaView edges={['top']} style={styles.container}>
+      <WebView source={{ uri: 'http://4.233.210.164' }} style={styles.webview} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
   webview: {
     flex: 1,
   },
